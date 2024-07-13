ba9b303b182367dca05efac6fc28cd5c3a9b99df
Aleksei Durnov <durnov.an@phystech.edu>
2024-07-13 18:35:41 +0300

    Sprint 1 (#1)
    * feature: add varjoin
    
    * feature: add speller
    
    * feature: add hotels
    
    * sprint_1: add func spellMinus
    
    ---------
    
    Co-authored-by: panterrich <panterrich@ispras.ru>

--- sprint-1/hotelbusiness/hotels.go
+++ sprint-1/hotelbusiness/hotels.go
@@ -2,6 +2,11 @@
 
 package hotelbusiness
 
+import (
+	"cmp"
+	"slices"
+)
+
 type Guest struct {
 	CheckInDate  int
 	CheckOutDate int
@@ -13,5 +18,31 @@ type Load struct {
 }
 
 func ComputeLoad(guests []Guest) []Load {
-	return []Load{}
+	if len(guests) == 0 {
+		return []Load{}
+	}
+
+	lastDate := slices.MaxFunc(guests, func(a, b Guest) int {
+		return cmp.Compare(a.CheckOutDate, b.CheckOutDate)
+	}).CheckOutDate
+
+	days := make([]Load, lastDate+1)
+
+	for _, g := range guests {
+		for i := g.CheckInDate; i < g.CheckOutDate; i++ {
+			days[i].GuestCount++
+		}
+	}
+
+	var load []Load
+	lastLoad := 0
+
+	for i, day := range days {
+		if lastLoad != day.GuestCount {
+			load = append(load, Load{StartDate: i, GuestCount: day.GuestCount})
+			lastLoad = day.GuestCount
+		}
+	}
+
+	return load
 }
