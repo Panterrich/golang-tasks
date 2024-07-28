f65c3e1ec289c84f09d8e9683fbe226797f70d32
Aleksei Durnov <durnov.an@phystech.edu>
2024-07-29 00:20:58 +0300

    Sprint 3 (#3)
    * [sprint-3] add areacalc
    
    * [sprint-3] add testequal
    
    * [sprint-3] rework switch
    
    ---------
    
    Co-authored-by: panterrich <panterrich@ispras.ru>

--- sprint-3/areacalc/areacalc.go
+++ sprint-3/areacalc/areacalc.go
@@ -1,5 +1,9 @@
 package areacalc
 
+import (
+	"strings"
+)
+
 const pi = 3.14159
 
 type Shape interface {
@@ -8,26 +12,59 @@ type Shape interface {
 }
 
 type Rectangle struct {
-	// TODO: implement me
+	a float64
+	b float64
+
+	shape string
 }
 
-func NewRectangle(float64, float64, string) *Rectangle {
-	// TODO: implement me
-	return &Rectangle{}
+func NewRectangle(a float64, b float64, shape string) *Rectangle {
+	return &Rectangle{
+		a:     a,
+		b:     b,
+		shape: shape,
+	}
 }
 
-// TODO: implement me
+func (r Rectangle) Area() float64 {
+	return r.a * r.b
+}
+
+func (r Rectangle) Type() string {
+	return r.shape
+}
 
 type Circle struct {
-	// TODO: implement me
+	r float64
+
+	shape string
 }
 
-func NewCircle(float64, string) *Circle {
-	// TODO: implement me
-	return &Circle{}
+func NewCircle(r float64, shape string) *Circle {
+	return &Circle{
+		r:     r,
+		shape: shape,
+	}
+}
+
+func (c Circle) Area() float64 {
+	return pi * c.r * c.r
+}
+
+func (c Circle) Type() string {
+	return c.shape
 }
 
 func AreaCalculator(figures []Shape) (string, float64) {
-	// TODO: implement me
-	return "", 0.0
+	var (
+		messages  []string
+		totalArea float64
+	)
+
+	for _, figure := range figures {
+		messages = append(messages, figure.Type())
+		totalArea += figure.Area()
+	}
+
+	return strings.Join(messages, "-"), totalArea
 }
--- sprint-3/testequal/assertions.go
+++ sprint-3/testequal/assertions.go
@@ -2,13 +2,87 @@
 
 package testequal
 
+import (
+	"bytes"
+	"maps"
+	"slices"
+)
+
+func checkEqual(expected, actual interface{}) bool {
+	switch e := expected.(type) {
+	case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64:
+		return expected == actual
+	case string:
+		if a, ok := actual.(string); ok && e == a {
+			return true
+		}
+	case map[string]string:
+		if a, ok := actual.(map[string]string); ok {
+			if e == nil && a == nil {
+				return true
+			}
+
+			if e != nil && a != nil && maps.Equal(e, a) {
+				return true
+			}
+
+			return false
+		}
+	case []int:
+		if a, ok := actual.([]int); ok {
+			if e == nil && a == nil {
+				return true
+			}
+
+			if e != nil && a != nil && slices.Equal(e, a) {
+				return true
+			}
+
+			return false
+		}
+	case []byte:
+		if a, ok := actual.([]byte); ok {
+			if e == nil && a == nil {
+				return true
+			}
+
+			if e != nil && a != nil && bytes.Equal(e, a) {
+				return true
+			}
+
+			return false
+		}
+	}
+
+	return false
+}
+
+func printMsgAndArgs(t T, msgAndArgs ...interface{}) {
+	t.Helper()
+
+	if len(msgAndArgs) == 0 {
+		t.Errorf("")
+	} else if len(msgAndArgs) == 1 {
+		t.Errorf(msgAndArgs[0].(string))
+	} else {
+		t.Errorf(msgAndArgs[0].(string), msgAndArgs[1:]...)
+	}
+}
+
 // AssertEqual checks that expected and actual are equal.
 //
 // Marks caller function as having failed but continues execution.
 //
 // Returns true iff arguments are equal.
 func AssertEqual(t T, expected, actual interface{}, msgAndArgs ...interface{}) bool {
-	panic("implement me")
+	if checkEqual(expected, actual) {
+		return true
+	}
+
+	t.Helper()
+	printMsgAndArgs(t, msgAndArgs...)
+
+	return false
 }
 
 // AssertNotEqual checks that expected and actual are not equal.
@@ -17,15 +91,36 @@ func AssertEqual(t T, expected, actual interface{}, msgAndArgs ...interface{}) b
 //
 // Returns true iff arguments are not equal.
 func AssertNotEqual(t T, expected, actual interface{}, msgAndArgs ...interface{}) bool {
-	panic("implement me")
+	if !checkEqual(expected, actual) {
+		return true
+	}
+
+	t.Helper()
+	printMsgAndArgs(t, msgAndArgs...)
+
+	return false
 }
 
 // RequireEqual does the same as AssertEqual but fails caller test immediately.
 func RequireEqual(t T, expected, actual interface{}, msgAndArgs ...interface{}) {
-	panic("implement me")
+	if checkEqual(expected, actual) {
+		return
+	}
+
+	t.Helper()
+	printMsgAndArgs(t, msgAndArgs...)
+
+	t.FailNow()
 }
 
 // RequireNotEqual does the same as AssertNotEqual but fails caller test immediately.
 func RequireNotEqual(t T, expected, actual interface{}, msgAndArgs ...interface{}) {
-	panic("implement me")
+	if !checkEqual(expected, actual) {
+		return
+	}
+
+	t.Helper()
+	printMsgAndArgs(t, msgAndArgs...)
+
+	t.FailNow()
 }
